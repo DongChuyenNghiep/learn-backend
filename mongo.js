56454d48e9a43251d745c28292f819064095ebdb
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-02-23 19:06:31 +0200

    Create mongo.js

---
+++ mongo.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose'
+import note from './models/note'
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+/*const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+}) */
+
+/* Note.find({important:true}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  }) */
+
+
+    const phoneSchema = new mongoose.Schema({
+        name : String,
+        number : String,
+    })
+    const phoneNote = mongoose.model('phonebook',phoneSchema,'phonebook')
+    const addphone = new phoneNote({
+        name: process.argv[3],
+        number:process.argv[4]
+    })
+    addphone.save().then(result=>{
+        console.log(`Added ${result.name} ${result.number} to phonebook`)
+    })
+    phoneNote.find({}).then(result => {
+        console.log("phonebook:");
+        result.map(people=>{
+            console.log(`${people.name} ${people.number}`);
+        })
+        mongoose.connection.close()
+    })
+   
